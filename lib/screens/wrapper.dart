a156950d1ed0a208a20e08bab1c5b2a77e7308fb
Raphael Barreto <tnt.raphael@gmail.com>
2019-10-30 15:03:26 -0300

    Signing Out

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
+import 'package:brew_crew/screens/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:brew_crew/models/user.dart';
@@ -11,6 +12,10 @@ class Wrapper extends StatelessWidget {
     print(user);
     
     // return either Home or Authenticated widget
-    return Authenticate();
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
