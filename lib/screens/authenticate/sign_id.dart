f5504125699716aced4f304ce70172f7312d3778
Raphael Barreto <tnt.raphael@gmail.com>
2019-11-09 12:46:42 -0300

    Sign in With Email & Password

--- lib/screens/authenticate/sign_id.dart
+++ lib/screens/authenticate/sign_id.dart
@@ -13,10 +13,13 @@ class SigIn extends StatefulWidget {
 class _SigInState extends State<SigIn> {
 
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // text field state
   String email = '';
   String password = '';
+  String error = '';
+
 
   @override
   Widget build(BuildContext context) {
@@ -38,10 +41,12 @@ class _SigInState extends State<SigIn> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                  setState(() => email = val);
                 }
@@ -49,6 +54,7 @@ class _SigInState extends State<SigIn> {
               SizedBox(height: 20.0),
               TextFormField(
                 obscureText: true,
+                validator: (val) => val.length < 6 ? 'Enter a password 6+ chars long' : null,
                 onChanged: (val) {
                   setState(() => password = val);
                 },
@@ -61,10 +67,19 @@ class _SigInState extends State<SigIn> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState.validate()) {
+                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
+                    if (result == null) {
+                      setState(() => error = 'Could not Sign In with those credentials');
+                    }
+                  }
                 },
               ),
+              SizedBox(height: 20.0,),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14.0),
+              )
             ],
           ),
         ),
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -28,7 +28,17 @@ class AuthService {
     }
   }
 
-  // sing in with email & password
+  // sign in with email & password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   // register with email & password
   Future registerWithEmailAndPassword(String email, String password) async {
